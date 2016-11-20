2c5549a4056d52c3db8f5613556faef6303d5ab5
Kris Reese <kris.reese@wwt.com>
2016-11-19 19:26:37 -0600

    initial commit

---
+++ lib/puppet/type/nssm_set.rb
@@ -0,0 +1,32 @@
+Puppet::Type.newtype(:nssm_set) do
+  desc "Puppet type to set service parameters"
+
+  newparam(:service_name, :namevar => true) do
+    desc "The name of the service NSSM is to manage"
+  end
+
+  newproperty(:create_user) do
+    desc "Whether or not to create local user account"
+    newvalues(:true, :false)
+    defaultto :false
+  end
+
+  newproperty(:service_user) do
+    desc "Username underwhich to run the service"
+    defaultto "LocalSystem"
+  end
+
+  newproperty(:service_pass) do
+    desc "Password for the :service_user"
+  end
+
+  newproperty(:service_interactive) do
+    desc "Whether or not to run service interactively"
+    newvalues(:true, :false)
+    defaultto :false
+  end
+
+  newproperty(:app_parameters) do
+    desc "Set additional app parameters"
+  end
+end
